b074ca5bdcfa4113eae95bf63be7b5ff6da0e709
chocolateboy <chocolate@cpan.org>
2020-04-20 16:27:20 +0100

    v3.0.0
    **Breaking Changes**
    
    - new required permission: `GM_listValues`
    - rename options.debug (default: false) -> options.cache (default: true)
    - remove `$.highlight.class` alias for `$.highlight.className`
    - update code from ES5 -> ES6
    - the `$.highlight` method is now async
    
    **Features**
    
    - all cached IDs are now encrypted for privacy
    - add options.debug (default: false) to enable logging for some methods
    - add changelog

--- src/highlighter.js
+++ src/highlighter.js
@@ -1,135 +1,236 @@
-// required: jQuery, GM_deleteValue, GM_getValue, GM_registerMenuCommand, GM_setValue
+// required:
+//
+//   - jQuery
+//   - GM_deleteValue
+//   - GM_getValue
+//   - GM_listValues
+//   - GM_registerMenuCommand
+//   - GM_setValue
 
-jQuery.highlight = (function ($) {
-    var CLASS          = 'github-com-chocolateboy-jquery-highlighter-highlighted';
-    var DEFAULT_ID     = 'id';
-    var DEFAULT_TARGET = function () { return $(this) }; // i.e. $item
-    var DEFAULT_TTL    = { days: 7 };
-    var DEFAULT_COLOR  = '#FFFD66';
-    var KEY            = 'seen';
+import { sha256 as encrypt } from 'crypto-hash'
 
-    // time-to-live: how long (in seconds) to cache IDs for
-    var TTL = (function (ttl) {
-        ttl.second = ttl.seconds = 1;
-        ttl.minute = ttl.minutes = 60 * ttl.seconds;
-        ttl.hour   = ttl.hours   = 60 * ttl.minutes;
-        ttl.day    = ttl.days    = 24 * ttl.hours;
-        ttl.week   = ttl.weeks   = 7  * ttl.days;
-        return ttl;
-    })({});
+const CLASS          = 'jquery-highlighter-highlighted'
+const COMMAND_NAME   = GM_info.script.name + ': clear data'
+const DEBUG          = { enabled: false }
+const DEFAULT_COLOR  = '#FFFD66'
+const DEFAULT_ID     = 'id'
+const DEFAULT_TARGET = function () { return $(this) } // i.e. $item
+const DEFAULT_TTL    = { days: 7 }
+const SCRIPT_VERSION = GM_info.script.version
+
+// XXX copied (with adjustments) from IMDb Tomatoes [1]
+//
+// the version of each cached record is a combination of the schema version
+// and the <major> part of the script's (SemVer) version e.g. 1 (schema
+// version) + 3.0.0 (script version) gives a version of "1/3"
+//
+// this means cached records are invalidated either a) when the schema
+// changes or b) when the major version (i.e. not the minor or patch
+// version) of the script changes
+//
+// [1] https://greasyfork.org/en/scripts/15222-imdb-tomatoes
+const SCHEMA_VERSION = 1
+const DATA_VERSION = SCHEMA_VERSION + '/' + SCRIPT_VERSION.split('.')[0] // e.g. 1/3
+
+// time-to-live: how long (in seconds) to cache IDs for
+const TTL = (ttl => {
+    ttl.second = ttl.seconds = 1
+    ttl.minute = ttl.minutes = 60 * ttl.seconds
+    ttl.hour   = ttl.hours   = 60 * ttl.minutes
+    ttl.day    = ttl.days    = 24 * ttl.hours
+    ttl.week   = ttl.weeks   = 7  * ttl.days
+    return ttl
+})({})
+
+/**************************** helper functions ****************************/
+
+// fetch the supplied ID from the cache. automatically handles encryption.
+// returns true if the ID is found, false otherwise
+async function cacheHas (id) {
+    const key = await encrypt(id)
+    return !!GM_getValue(key)
+}
+
+// add an { expires, version } value to the cache under the supplied ID
+// (encrypted)
+async function cacheSet (id, expires) {
+    const date = new Date(expires).toLocaleString()
+    const key = await encrypt(id)
+    const value = { expires, version: DATA_VERSION }
+    const json = JSON.stringify(value)
+
+    debug(`caching ${id} (${key}) until ${date}`)
+    GM_setValue(key, json)
+}
+
+// log a debug message to the console if debugging is enabled (via
+// options.debug)
+function debug (...args) {
+    if (DEBUG.enabled) {
+        console.warn(...args)
+    }
+}
+
+// purge expired cache entries (encrypted IDs)
+// borrowed from IMDb Tomatoes (see above)
+function purgeCached (date) {
+    for (const key of GM_listValues()) {
+        const value = GM_getValue(key)
+        const cached = JSON.parse(value)
+
+        let $delete = true
+
+        if (date === -1) {
+            debug(`purging value (forced): ${key}`)
+        } else if ((typeof cached.expires !== 'number') || (typeof cached.version !== 'string')) {
+            debug(`purging invalid value: ${key}:`, cached)
+        } else if (date > cached.expires) {
+            const expired = new Date(cached.expires).toLocaleString()
+            debug(`purging expired value: ${key} (${expired})`)
+        } else if (cached.version !== DATA_VERSION) {
+            debug(`purging obsolete value: ${key} (${cached.version})`)
+        } else {
+            $delete = false
+        }
+
+        if ($delete) {
+            GM_deleteValue(key)
+        }
+    }
+}
+
+// a uniform way to jQuery-select an element/elements. allows a selector to be
+// defined as a string (jQuery selector) or a function which returns a jQuery
+// collection
+//
+// - name: the name (string) of the thing being selected (e.g. item or ID);
+//   used for diagnostics
+// - selector: a string (jQuery.Selector) or a function which is called
+//   with the supplied `this` value (context) and arguments
+// - context (optional): the `this` value to assign when calling the
+//   function, or the second argument to pass to $(selector, context);
+//   defaults to window.document if not supplied
+// - args (optional): an array of additional arguments to pass when the
+//   selector is a function
+function select (name, selector, options = {}) {
+    const context = options.context || document
+    const type = typeof selector
+
+    if (type === 'function') {
+        return selector.apply(context, options.args || [])
+    } else if (type === 'string') {
+        return $(selector, context)
+    } else {
+        throw new TypeError(`invalid ${name} selector: expected string or function, got: ${type}`)
+    }
+}
+
+// take a TTL spec (object) and convert it into a duration in milliseconds
+function ttlToMilliseconds (ttl) {
+    let seconds = 0
+
+    for (const key in ttl) {
+        seconds += ttl[key] * (TTL[key] || 0)
+    }
+
+    return seconds * 1000
+}
+
+/************************************ main ************************************/
+
+const highlightFor = $ => async function highlight (options) {
+    const NOW = Date.now()
+
+    // if falsey, the cache is neither read from nor written to.
+    // this allows highlighters to be modified and reloaded
+    // without having to manually clear the cache every time
+    const useCache = 'cache' in options ? !!options.cache : true
+
+    // if set to a falsey value, don't deduplicate article IDs i.e. *do*
+    // highlight duplicate links
+    const highlightDuplicateLinks = 'dedup' in options ? !options.dedup : false
+
+    // function which returns true if an article ID is cached, false
+    // otherwise; handles ID encryption
+    const seen = useCache ? cacheHas : () => false
+
+    // time-to-live: how long (in milliseconds) to cache IDs for
+    const ttl = ttlToMilliseconds(options.ttl || DEFAULT_TTL)
+
+    // the background color of the target element(s)
+    const color = options.color || DEFAULT_COLOR
+
+    // selector (string or function) for the element to highlight
+    const targetSelector = options.target || DEFAULT_TARGET
+
+    // attribute name (string) or function to select a unique
+    // identifier for each item
+    const idSelector = options.id || DEFAULT_ID
+
+    // selector (string or function) for the article/story
+    const itemSelector = options.item
+
+    // optional callback function called after the target has been highlighted
+    const onHighlight = options.onHighlight || function () {}
+
+    // enable/disable the debug function to log (some) diagnostic messages
+    // to the console
+    DEBUG.enabled = 'debug' in options ? !!options.debug : false
+
+    // helper function which extracts an item's unique ID
+    let getId
+
+    if (typeof idSelector === 'function') {
+        getId = (item, args) => select('id', idSelector, { context: item, args })
+    } else if (typeof idSelector === 'string') {
+        getId = item => $(item).attr(idSelector)
+    } else {
+        throw new TypeError(`invalid ID selector: expected string or function, got: ${idSelector}`)
+    }
+
+    if (typeof itemSelector !== 'string' && typeof itemSelector !== 'function') {
+        throw new TypeError(`invalid item selector: expected string or function, got: ${itemSelector}`)
+    }
+
+    // highlight the selected articles/stories
+    async function processItems ($items) {
+        for (const item of $items) {
+            const $target = select('target', targetSelector, { context: item })
+
+            const id = getId(item, [$target])
+            const cached = await seen(id)
+
+            if (!cached || highlightDuplicateLinks) {
+                $target.css('background-color', color)
+                $target.addClass(CLASS)
+                onHighlight.call(item, $target, { id: id, color: color })
+            }
+
+            if (!cached) {
+                cacheSet(id, NOW + ttl)
+            }
+        }
+    }
 
     // register this early so data can be cleared even if there's an error
-    var commandName = GM_info.script.name + ': clear data';
+    GM_registerMenuCommand(COMMAND_NAME, () => { purgeCached(-1) })
 
-    GM_registerMenuCommand(commandName, function () { GM_deleteValue(KEY) });
+    // remove expired cache entries
+    purgeCached(NOW)
 
-    function ttlToMilliseconds (ttl) {
-        var seconds = 0, key;
+    const $document = $(document)
 
-        for (key in ttl) {
-            seconds += ttl[key] * (TTL[key] || 0);
-        }
-
-        return seconds * 1000;
+    // if the jQuery-onMutate plugin is loaded
+    if ($document.onCreate && (typeof itemSelector === 'string')) {
+        // handle dynamically-created items (includes statically-defined items)
+        $document.onCreate(itemSelector, processItems, true /* multi */)
+    } else {
+        // handle statically-defined items
+        const $items = select('item', itemSelector)
+        await processItems($items)
     }
+}
 
-    function select (name, selector, _context, args) {
-        var context = _context || document;
-        var type = typeof selector;
-
-        if (type === 'function') {
-            return selector.apply(context, args || []);
-        } else if (type === 'string') {
-            return $(selector, context);
-        } else {
-            throw new TypeError('invalid ' + name + ' selector: expected string or function, got: ' + type);
-        }
-    }
-
-    function highlight (options) {
-        // if truthy, the cache is neither read from nor written to.
-        // this allows highlighters to be modified and reloaded
-        // without having to manually clear the cache every time
-        // FIXME rename this `debug = true` -> `cache = false`
-        var debug = options.debug;
-
-        // if set to a falsey value, don't deduplicate article IDs i.e.
-        // highlight duplicate links
-        var highlightDuplicateLinks = ('dedup' in options) ? !options.dedup : false;
-
-        // article ID -> cache expiry timestamp (epoch milliseconds)
-        var seen = debug ? {} : JSON.parse(GM_getValue(KEY, '{}'));
-
-        // time-to-live: how long (in milliseconds) to cache IDs for
-        var ttl = ttlToMilliseconds(options.ttl || DEFAULT_TTL);
-
-        // the background color of the target element(s)
-        var color = options.color || DEFAULT_COLOR;
-
-        // selector (string or function) for the element to highlight
-        var targetSelector = options.target || DEFAULT_TARGET;
-
-        // attribute name (string) or function to select a unique
-        // identifier for the item
-        var idSelector = options.id || DEFAULT_ID;
-
-        // selector (string or function) for the article/story &c.
-        var itemSelector = options.item;
-
-        // optional callback function called after the target has been highlighted
-        var onHighlight = options.onHighlight || function () {};
-
-        // helper function which extracts an item's unique ID
-        var getId = (typeof idSelector === 'function') ?
-            function (item, args) { return select('id', idSelector, item, args) } :
-            function (item) { return $(item).attr(idSelector) };
-
-        // the current date/time in epoch milliseconds
-        var now = new Date().getTime();
-
-        // highlight the selected articles/stories
-        function processItems ($items) {
-            $items.each(function () {
-                var $target = select('target', targetSelector, this);
-                var id = getId(this, [$target]);
-
-                if (!seen[id] || highlightDuplicateLinks) {
-                    $target.css('background-color', color);
-                    $target.addClass(CLASS);
-                    onHighlight.call(this, $target, { id: id, color: color });
-                }
-
-                if (!seen[id]) {
-                    seen[id] = now + ttl;
-                }
-            });
-
-            if (!debug) {
-                GM_setValue(KEY, JSON.stringify(seen));
-            }
-        }
-
-        // purge expired IDs
-        for (var id in seen) {
-            if (now > seen[id]) {
-                delete seen[id];
-            }
-        }
-
-        var $document = $(document);
-
-        // if the jQuery-onMutate plugin is loaded
-        if ($document.onCreate && (typeof itemSelector === 'string')) {
-            // handle dynamically-created items (includes statically-defined items)
-            $document.onCreate(itemSelector, processItems, true /* multi */);
-        } else {
-            // handle statically-defined items
-            processItems(select('item', itemSelector));
-        }
-    }
-
-    highlight.className = highlight['class'] = CLASS;
-    highlight.selector = '.' + CLASS;
-
-    return highlight;
-}(jQuery));
+jQuery.highlight = highlightFor(jQuery)
+jQuery.highlight.className = CLASS
+jQuery.highlight.selector = '.' + CLASS
